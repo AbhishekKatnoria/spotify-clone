38ee0a16709aebf82111cb816df108894756ccb0
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-04-30 23:34:46 +0530

    setup the files and add login  component

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className='container'>
+        hello I am login
+    </div>
+  )
+}
+
+export default Login
